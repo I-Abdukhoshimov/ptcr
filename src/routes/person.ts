6e5d5c8a778ed041e387bf10a52dab0b5eaeae52
an-ikhtiyour <an.ikhtiyour@gmail.com>
2022-06-11 00:42:21 +0500

    some changes

--- src/routes/person.ts
+++ src/routes/person.ts
@@ -1,8 +1,12 @@
 import {Router } from "express";
 import { PersonController } from "../controllers/person";
 
+
+//router
 const router  = Router({mergeParams:true})
 const  controller = new PersonController()
+
+
 
 // new comment
 router.route("/").get(controller.getAll).post(controller.create)
